7f96b1c59805b92b152227f38af60f09ee4f0b9b
Mircea Popescu <mirceap@gmail.com>
2018-10-10 14:01:56 -0500

    Initial Commit

---
+++ BreakOutDemo/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BreakOutDemo
+//
+//  Created by Mircea Popescu on 10/10/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ BreakOutDemo/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  BreakOutDemo
+//
+//  Created by Mircea Popescu on 10/10/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
